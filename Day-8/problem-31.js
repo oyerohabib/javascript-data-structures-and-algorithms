8de4d3e2f19e718bf40d8c7e8e1e76d025236e7f
oyerohabib <oyerohabib@yahoo.com>
2023-07-26 09:22:00 +0100

    drop it

--- Day-8/problem-31.js
+++ Day-8/problem-31.js
@@ -1,7 +1,41 @@
-function smallestCommons(arr) {
-  // Setup
-  const [min, max] = arr.sort((a, b) => a - b);
-  const range = Array(max - min + 1)
-    .fill(0)
-    .map((_, i) => i + min);
+// Drop it
+// solution 1
+function dropElements(arr, func) {
+  let i = arr.indexOf(arr.find(func));
+  return i === -1 ? [] : arr.slice(i);
 }
+
+console.log(
+  dropElements([1, 2, 3, 4], function (n) {
+    return n > 5;
+  })
+);
+
+// solution 2
+function dropElements_1(arr, func) {
+  while (arr.length > 0 && !func(arr[0])) {
+    arr.shift();
+  }
+  return arr;
+}
+
+console.log(
+  dropElements_1([1, 2, 3, 4], function (n) {
+    return n > 5;
+  })
+);
+
+// solution 3
+function dropElements_2(arr, func) {
+  let i = 0;
+  while (i < arr.length && !func(arr[i])) {
+    i++;
+  }
+  return i === arr.length ? [] : arr.slice(i);
+}
+
+console.log(
+  dropElements_2([1, 2, 3, 4], function (n) {
+    return n > 5;
+  })
+);
