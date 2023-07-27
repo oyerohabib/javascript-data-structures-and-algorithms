481f0ef424d422581245b8ab8d3229a07b667d83
oyerohabib <oyerohabib@yahoo.com>
2023-07-27 15:32:19 +0100

    update: updated palindrom checker

--- Day-9/problem-38.js
+++ Day-9/problem-38.js
@@ -18,3 +18,15 @@ console.log(palindrome("2A3 3a2"));
 console.log(palindrome("2_A3*3#A2"));
 console.log(palindrome("eye"));
 console.log(palindrome("race car"));
+
+function palindrome_1(str) {
+  const cleanStr = str.replace(/[\W_]/g, "").toLowerCase();
+  const reversedString = cleanStr.split("").reverse().join("");
+  return cleanStr === reversedString;
+}
+
+console.log(palindrome_1("2A3*3a2"));
+console.log(palindrome_1("2A3 3a2"));
+console.log(palindrome_1("2_A3*3#A2"));
+console.log(palindrome_1("eye"));
+console.log(palindrome_1("race car"));
