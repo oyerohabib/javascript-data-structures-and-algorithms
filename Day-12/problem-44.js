918b09f43e475f1e94daf9d7fa805a6f4c804bea
oyerohabib <oyerohabib@yahoo.com>
2023-08-10 11:30:37 +0100

    updated: Inventory Update solved

--- Day-12/problem-44.js
+++ Day-12/problem-44.js
@@ -6,10 +6,13 @@ function updateInventory(arr1, arr2) {
 
   // Sort the array by item name using a custom comparison function
   const updatedInventory = concatenatedArray.sort((a, b) => {
+    // a[1].localeCompare(b[1]) - alternative to the below.
     if (a[1] < b[1]) return -1;
     if (a[1] > b[1]) return 1;
     return 0;
   });
+  //   [[21, "Bowling Ball"], [67, "Bowling Ball"], [2, "Dirty Sock"], [1, "Hair Pin"],
+  //   [2, "Hair Pin"], [3, "Half-Eaten Apple"], [5, "Microphone"], [7, "Toothpaste"]];
 
   // Merge items with the same name and update quantities
   const mergedInventory = [];
@@ -43,48 +46,6 @@ const newInv_1 = [
 ];
 
 console.log(updateInventory(curInv_1, newInv_1));
-
-// solution 2
-function updateInventory_1(arr1, arr2) {
-  // Concatenate the arrays and sort them by item name
-  const concatenatedArray = arr1.concat(arr2);
-  const updatedInventory = concatenatedArray.sort((a, b) =>
-    a[1].localeCompare(b[1])
-  );
-
-  // Merge items with the same name and update quantities
-  const mergedInventory = [];
-  let currentItem = updatedInventory[0];
-  for (let i = 1; i < updatedInventory.length; i++) {
-    if (currentItem[1] === updatedInventory[i][1]) {
-      currentItem[0] += updatedInventory[i][0];
-    } else {
-      mergedInventory.push(currentItem);
-      currentItem = updatedInventory[i];
-    }
-  }
-  mergedInventory.push(currentItem);
-
-  return mergedInventory;
-}
-
-// Example inventory lists
-const curInv_2 = [
-  [21, "Bowling Ball"],
-  [2, "Dirty Sock"],
-  [1, "Hair Pin"],
-  [5, "Microphone"],
-];
-
-const newInv_2 = [
-  [2, "Hair Pin"],
-  [3, "Half-Eaten Apple"],
-  [67, "Bowling Ball"],
-  [7, "Toothpaste"],
-];
-
-const updatedInventory_1 = updateInventory_1(curInv_2, newInv_2);
-console.log(updatedInventory_1);
 
 // solution 3
 function updateInventory_2(arr1, arr2) {
